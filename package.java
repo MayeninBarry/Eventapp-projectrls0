da8ba2a843cb232f89f6274e3e78382c73f9a265
root <root@Alleyah>
2024-12-03 22:40:43 -0500

    my first commit job

---
+++ package.java
@@ -0,0 +1,18 @@
+public class Contributor {
+    private Long id;
+    private String name;
+    private String couple;
+    private Boolean isAttending;
+    private String familyName;
+    private String tableNumber;
+    private List<Child> children;
+    private String contact;
+}
+
+public class Child {
+    private String name;
+    private String gender;
+    private int age;
+    private String preferredGift;
+}
+
